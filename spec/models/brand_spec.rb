a46606341d2c6ab5e1de72ac5d9045338d55630c
Drew MacNicholas <dmacnicholas@gmail.com>
2022-07-07 22:32:42 -0500

    Add brand model validations

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -2,4 +2,10 @@ require 'rails_helper'
 
 RSpec.describe Brand, type: :model do
   it {should have_many(:cards) }
+
+  describe 'validations' do
+    it {should validate_presence_of(:name)}
+    it {should validate_presence_of(:year)}
+    it {should validate_presence_of(:sold_at_retail)}
+  end
 end
