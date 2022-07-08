52761930b3c968b7f3e6ae8ae8157cd8e511d54a
Drew MacNicholas <dmacnicholas@gmail.com>
2022-07-07 22:42:56 -0500

    Add Card Validation Test

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Card, type: :model do
   it {should belong_to :brand}
+
+  describe 'validations' do
+    it {should validate_presence_of(:player_name)}
+    it {should validate_presence_of(:description)}
+    it {should validate_presence_of(:graded)}
+    it {should validate_presence_of(:value)}
+  end
 end
