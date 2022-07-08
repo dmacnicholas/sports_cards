4d313f911d42a196ca38480692a70fb0f41965c7
Drew MacNicholas <dmacnicholas@gmail.com>
2022-07-07 22:44:13 -0500

    Add Card Validation Method

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,8 @@
 class Card < ApplicationRecord
   belongs_to :brand
+  validates :player_name, presence: true
+  validates :description, presence: true
+  validates :graded, presence: true
+  validates :value, presence: true
+
 end
