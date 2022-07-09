0ca51da4f7385130e0c03a764e897d25efa1cca8
Drew MacNicholas <dmacnicholas@gmail.com>
2022-07-09 08:16:53 -0500

    Refactor Brand Feature Tests

--- spec/features/brands/index_spec.rb
+++ spec/features/brands/index_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Brands Index' do
   before :each do
-    @prizm = Brand.create!(name: 'Prizm', year: 2022, sold_at_retail: false)
-    @mosaic = Brand.create!(name: 'Mosaic', year: 2022, sold_at_retail: true)
-    @donruss = Brand.create!(name: 'Donruss', year: 2022, sold_at_retail: true)
+    @brand_1 = Brand.create!(name: 'Prizm', year: 2022, sold_at_retail: false)
+    @brand_2 = Brand.create!(name: 'Mosaic', year: 2022, sold_at_retail: true)
+    @brand_3 = Brand.create!(name: 'Donruss', year: 2022, sold_at_retail: true)
   end
   # User Story 1, Parent Index
       # For each parent table
@@ -14,8 +14,8 @@ RSpec.describe 'Brands Index' do
   it 'shows all brands and their names' do
     visit '/brands'
 
-    expect(page).to have_content(@prizm.name)
-    expect(page).to have_content(@mosaic.name)
-    expect(page).to have_content(@donruss.name)
+    expect(page).to have_content(@brand_1.name)
+    expect(page).to have_content(@brand_2.name)
+    expect(page).to have_content(@brand_3.name)
   end
 end
--- spec/features/brands/show_spec.rb
+++ spec/features/brands/show_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Brands Show' do
   before :each do
-    @prizm = Brand.create!(name: 'Prizm', year: 2022, sold_at_retail: false)
-    @mosaic = Brand.create!(name: 'Mosaic', year: 2022, sold_at_retail: true)
-    @donruss = Brand.create!(name: 'Donruss', year: 2022, sold_at_retail: true)
+    @brand_1 = Brand.create!(name: 'Prizm', year: 2022, sold_at_retail: false)
+    @brand_2 = Brand.create!(name: 'Mosaic', year: 2022, sold_at_retail: true)
+    @brand_3 = Brand.create!(name: 'Donruss', year: 2022, sold_at_retail: true)
   end
   # User Story 2, Parent Show
    # As a visitor
@@ -12,16 +12,16 @@ RSpec.describe 'Brands Show' do
    # Then I see the parent with that id including the parent's attributes:
    # - data from each column that is on the parent table
   it 'shows all brands ids and attributes' do
-    visit "/brands/#{@prizm.id}"
+    visit "/brands/#{@brand_1.id}"
 
 
-    expect(page).to have_content(@prizm.name)
-    expect(page).to have_content(@prizm.year)
-    expect(page).to have_content(@prizm.sold_at_retail)
-    expect(page).to have_content(@prizm.created_at)
-    expect(page).to have_content(@prizm.updated_at)
+    expect(page).to have_content(@brand_1.name)
+    expect(page).to have_content(@brand_1.year)
+    expect(page).to have_content(@brand_1.sold_at_retail)
+    expect(page).to have_content(@brand_1.created_at)
+    expect(page).to have_content(@brand_1.updated_at)
 
-    expect(page).to_not have_content(@mosaic.name)
+    expect(page).to_not have_content(@brand_2.name)
 
 
   end
