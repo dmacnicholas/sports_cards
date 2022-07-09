e0165c5a0a281aa334c1273b1f259d3b8e254571
Drew MacNicholas <dmacnicholas@gmail.com>
2022-07-09 09:11:02 -0500

    Add Card Index Method

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/brands', to: 'brands#index'
   get '/brands/:id', to: 'brands#show'
+  get '/cards', to: 'cards#index'
 end
