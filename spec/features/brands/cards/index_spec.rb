ee80f1401b87e80f4680e32581acdabf3faceb95
Drew MacNicholas <dmacnicholas@gmail.com>
2022-07-10 18:48:09 -0500

    Add link to brand index test

--- spec/features/brands/cards/index_spec.rb
+++ spec/features/brands/cards/index_spec.rb
@@ -48,11 +48,11 @@ RSpec.describe 'Brand Cards Index' do
    # As a visitor
    # When I visit any page on the site
    # Then I see a link at the top of the page that takes me to the Child Index
- it 'has link to card index' do
-   visit "/brands/#{@brand_1.id}/cards"
-   expect(page).to have_link('Cards Index')
-   click_link 'Cards Index'
-   expect(current_path).to eq('/cards')
-   # save_and_open_page
- end
+  it 'has link to card index' do
+    visit "/brands/#{@brand_1.id}/cards"
+    expect(page).to have_link('Cards Index')
+    click_link 'Cards Index'
+    expect(current_path).to eq('/cards')
+    # save_and_open_page
+  end
 end
