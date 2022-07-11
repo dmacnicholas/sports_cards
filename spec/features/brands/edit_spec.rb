4e3c822e99371e4bfc7dfe9b2ff9d6c96c349e1e
Drew MacNicholas <dmacnicholas@gmail.com>
2022-07-11 15:04:26 -0500

    Add test for editing brands

---
+++ spec/features/brands/edit_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe 'Brands New' do
+  before :each do
+    @brand_1 = Brand.create!(name: 'Prizm', year: 2022, sold_at_retail: false)
+    @brand_2 = Brand.create!(name: 'Mosaic', year: 2022, sold_at_retail: true)
+    @brand_3 = Brand.create!(name: 'Donruss', year: 2022, sold_at_retail: true)
+    @card_1 = @brand_1.cards.create!(player_name: 'LeBron James', description: 'Red Fast Break', graded: true, value: 250.00)
+    @card_2 = @brand_1.cards.create!(player_name: 'Luka Doncic', description: 'Blue Fast Break', graded: false, value: 135.00)
+    @card_3 = @brand_1.cards.create!(player_name: 'Ja Morant', description: 'Purple Ice Auto', graded: true, value: 900.00)
+  end
+  # User Story 12, Parent Update
+    # As a visitor
+    # When I visit a parent show page
+    # Then I see a link to update the parent "Update Parent"
+    # When I click the link "Update Parent"
+    # Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
+    # When I fill out the form with updated information
+    # And I click the button to submit the form
+    # Then a `PATCH` request is sent to '/parents/:id',
+    # the parent's info is updated,
+    # and I am redirected to the Parent's Show page where I see the parent's updated info
+  it 'can update brand' do
+   visit "/brands/#{@brand_1.id}/edit"
+   fill_in 'Name', with: 'Chronicles'
+   fill_in 'Year', with: '2021'
+   check 'Sold at retail'
+   click_button 'Update Brand'
+
+   expect(current_path).to eq("/brands/#{@brand_1.id}")
+   expect(page).to have_content('Chronicles')
+   expect(page).to have_content('2021')
+   expect(page).to have_content('Sold at retail')
+ end
+end
